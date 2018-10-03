b207852f495c76833900d2ab345d27a405cf95f7
Gordiig <gordiig@gmail.com>
2018-10-04 01:07:56 +0300

    Added chatVC

---
+++ Swimple-chat-iOS/Swimple-chat-iOS/VCs/ChatViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ChatViewController.swift
+//  Swimple-chat-iOS
+//
+//  Created by Dmitry Gorin on 04/10/2018.
+//  Copyright © 2018 gordiig. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController
+{
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var msgTextField: UITextView!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        msgTextField.layer.cornerRadius = 10
+    }
+    
+}
