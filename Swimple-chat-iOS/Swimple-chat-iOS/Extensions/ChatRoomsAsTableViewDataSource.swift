38e21c6fd548ef5bb32ae1649e7bf5d84c9327b5
Gordiig <gordiig@gmail.com>
2018-10-21 15:40:40 +0300

    Added extension for ChatMessages for UITableViewDataSource

---
+++ Swimple-chat-iOS/Swimple-chat-iOS/Extensions/ChatRoomsAsTableViewDataSource.swift
@@ -0,0 +1,30 @@
+//
+//  ChatRoomsAsTableViewDataSource.swift
+//  Swimple-chat-iOS
+//
+//  Created by Dmitry Gorin on 21/10/2018.
+//  Copyright © 2018 gordiig. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+extension ChatRooms: UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return ChatRooms.rooms.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ChatListTableViewCell") as? ChatListTableViewCell else
+        {
+            return UITableViewCell()
+        }
+        
+        let room = ChatRooms.rooms[indexPath.row]
+        cell.configure(username: room.interlocutor, lastMessage: room.messages.last?.msg ?? "<#NO MESSAGES#>")
+    }
+}
