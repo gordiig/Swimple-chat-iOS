7fd41da89e3b7ef01eef3fa0793d28b2438fb492
Gordiig <gordiig@gmail.com>
2018-10-21 19:12:34 +0300

    Added Notification.Name ext

---
+++ Swimple-chat-iOS/Swimple-chat-iOS/Extensions/NotificationNameExtensions.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationNameExtensions.swift
+//  Swimple-chat-iOS
+//
+//  Created by Dmitry Gorin on 21/10/2018.
+//  Copyright © 2018 gordiig. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name
+{
+    static let chatRoomsWereChanged = Notification.Name("chatRoomsWereChanged")
+}
