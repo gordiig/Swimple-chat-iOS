21473e0b1b49c2e1e2dfb49ae4512b021e3ac09f
Gordiig <gordiig@gmail.com>
2018-11-01 16:24:35 +0300

    Add permissions checking

--- Swimple-chat-iOS/Swimple-chat-iOS/Models/FrameExtractor.swift
+++ Swimple-chat-iOS/Swimple-chat-iOS/Models/FrameExtractor.swift
@@ -12,31 +12,52 @@ import AVFoundation
 class FrameExtractor: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate
 {
     private let captureSession = AVCaptureSession()
-    private var videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+    private let backVideoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+    private let frontVideoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front)
     private let micDevice = AVCaptureDevice.default(.builtInMicrophone, for: .audio, position: .unspecified)
-    private var videoDeviceInput: AVCaptureDeviceInput!
+    private var backVideoDeviceInput: AVCaptureDeviceInput!
+    private var frontVideoDeviceInput: AVCaptureDeviceInput!
     private var micDeviceInput: AVCaptureDeviceInput!
     private let extractorQueue = DispatchQueue(label: "extractorQueue")
     
-    override init()
+    private weak var alertDelegate: MyViewController!
+    
+    init?(devices: [AVCaptureDevice], alertDelegate: MyViewController)
     {
         super.init()
+        self.alertDelegate = alertDelegate
+        
+        guard askPermissions(for: .video) else
+        {
+            alertDelegate.alert(title: "Permissions error!", message: "Camera permissions denied!")
+            return nil
+        }
+        guard askPermissions(for: .audio) else
+        {
+            alertDelegate.alert(title: "Permissions error!", message: "Microphone permissions denied!")
+            return nil
+        }
+        
+        self.configureAV()
     }
     
     private func askPermissions(for device: AVMediaType) -> Bool
     {
+        var ans = false
         switch AVCaptureDevice.authorizationStatus(for: device)
         {
         case .authorized:
-            return true
+            ans = true
         case .denied, .restricted:
-            return false
+            ans = false
         case .notDetermined:
             AVCaptureDevice.requestAccess(for: device)
-            { (result) in
-                return result
+            { result in
+                ans = result
             }
         }
+        
+        return ans
     }
     
     private func configureAV()
