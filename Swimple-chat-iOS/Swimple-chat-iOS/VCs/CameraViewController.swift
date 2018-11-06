3f1223b2d15eece451ffb1b0cc44c6dcae0d6a00
Gordiig <gordiig@gmail.com>
2018-11-06 13:52:59 +0300

    Added FrameExtractor to CameraViewController

--- Swimple-chat-iOS/Swimple-chat-iOS/VCs/CameraViewController.swift
+++ Swimple-chat-iOS/Swimple-chat-iOS/VCs/CameraViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AVFoundation
 
-class CameraViewController: MyViewController, AVCaptureVideoDataOutputSampleBufferDelegate
+class CameraViewController: MyViewController, FrameExtractorOutputDelegate
 {
     @IBOutlet weak var previewView: CameraPreviewView!
     @IBOutlet weak var dismissButton: UIButton!
@@ -18,12 +18,7 @@ class CameraViewController: MyViewController, AVCaptureVideoDataOutputSampleBuff
     
     override var prefersStatusBarHidden: Bool { return true } 
     
-    var captureSession = AVCaptureSession()
-    var videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
-    let micDevice = AVCaptureDevice.default(.builtInMicrophone, for: .audio, position: .unspecified)
-    var videoDeviceInput: AVCaptureDeviceInput!
-    var micDeviceInput: AVCaptureDeviceInput!
-    var videoDataPocessingQueue = DispatchQueue(label: "videoDataProcessingQueue")
+    var frameExtractor: FrameExtractor!
     
     override func viewDidLoad()
     {
@@ -32,125 +27,33 @@ class CameraViewController: MyViewController, AVCaptureVideoDataOutputSampleBuff
         self.changeCamBlurView.layer.cornerRadius = 10
         self.changeCamBlurView.clipsToBounds = true
         
-        self.configureCaptureSession()
-        self.previewView.videoPreviewLayer.session = self.captureSession
-        
-        captureSession.startRunning()
-    }
-    
-    
-    // MARK: - AVFoundation
-    func configureCaptureSession()
-    {
-        self.captureSession.beginConfiguration()
-        
-        self.checkPrivacyStatus(for: .video)
-        guard let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice!), captureSession.canAddInput(videoDeviceInput) else
+        guard let frameExtractor = FrameExtractor(alertDelegate: self) else
         {
-            self.alert(title: "Error configuring capture session", message: "Can't create videoDeviceInput!")
+            alert(title: "Error in camera", message: "Can't create frame extractor!")
+            self.dismiss(animated: true)
             return
         }
-        self.videoDeviceInput = videoDeviceInput
-        captureSession.addInput(videoDeviceInput)
-        
-        self.checkPrivacyStatus(for: .audio)
-        guard let micDeviceInput = try? AVCaptureDeviceInput(device: micDevice!), captureSession.canAddInput(micDeviceInput) else
-        {
-            self.alert(title: "Error configuring capture session", message: "Can't create micDeviceInput!")
-            return
-        }
-        self.micDeviceInput = micDeviceInput
-        captureSession.addInput(micDeviceInput)
-        
-        let outputDevice = AVCaptureVideoDataOutput()
-        outputDevice.setSampleBufferDelegate(self, queue: self.videoDataPocessingQueue)
-//        outputDevice.videoSettings = [AVVideoCodecKey: AVVideoCodecType.jpeg]
-        if captureSession.canAddOutput(outputDevice)
-        {
-            captureSession.addOutput(outputDevice)
-        }
-        
-        captureSession.commitConfiguration()
-    }
-    
-    func checkPrivacyStatus(for type: AVMediaType)
-    {
-        switch AVCaptureDevice.authorizationStatus(for: type)
-        {
-        case .authorized:
-            return
-            
-        case .denied:
-            self.alert(title: "Media type is denied", message: "Privacy status for media \(type.rawValue) is denied")
-            self.dismiss(animated: true, completion: nil)
-            
-        case .restricted:
-            self.alert(title: "Media type is denied", message: "Privacy status for media \(type.rawValue) is restricted")
-            self.dismiss(animated: true, completion: nil)
-            
-        case .notDetermined:
-            AVCaptureDevice.requestAccess(for: type)
-            { granted in
-                if granted
-                {
-                    return
-                }
-                else
-                {
-                    self.alert(title: "Media type is denied", message: "Privacy status for media \(type.rawValue) is denied")
-                    self.dismiss(animated: true, completion: nil)
-                }
-            }
-        }
-    }
-    
-    func changeCamera()
-    {
-        self.captureSession.beginConfiguration()
-        self.captureSession.removeInput(videoDeviceInput)
-        
-        if videoDevice?.position == .front
-        {
-            self.videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
-        }
-        else
-        {
-            self.videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front)
-        }
-        
-        guard let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice!), captureSession.canAddInput(videoDeviceInput) else
-        {
-            self.alert(title: "Error configuring capture session", message: "Can't create videoDeviceInput!")
-            return
-        }
-        self.videoDeviceInput = videoDeviceInput
-        captureSession.addInput(videoDeviceInput)
-        
-        self.captureSession.commitConfiguration()
+        self.frameExtractor = frameExtractor
+        self.frameExtractor.registerPreviewView(previewView)
     }
     
     
     // MARK: - Buttons
     @IBAction func changeCamButtonPressed(_ sender: Any)
     {
-        self.changeCamera()
+        self.frameExtractor.flipCamera()
     }
     
     @IBAction func dismissButtonPressed(_ sender: Any)
     {
-        captureSession.stopRunning()
+        self.frameExtractor.stop()
         self.dismiss(animated: true, completion: nil)
     }
     
     
-    // MARK: - Saving video data
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection)
+    // MARK: - FrameExtractorOutputDelegate
+    func frameExtractor(didOutputFrame frame: UIImage)
     {
-//        print("Captured output from \(connection.description)")
-    }
-    
-    func captureOutput(_ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection)
-    {
-        print("Captured output was dropped from connection \"\(connection.description)\"")
+        print("hello")
     }
 }
